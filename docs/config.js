fca68e8f91d6b409bf4a6fddb7c28ddf91f85159
tkohdev <166993858+tkohdev@users.noreply.github.com>
2025-01-10 10:20:45 +0900

    Add files via upload

---
+++ docs/config.js
@@ -0,0 +1,14 @@
+export default {
+    // Token Implicit Grant Client ID
+    clientID: 'aafaf879-6961-472a-b5dc-db4fb6a0bd68',
+
+    redirectUri: 'https://tkohdev.github.io/translate/index.heml',
+
+    genesysCloud: {
+        // Genesys Cloud region
+        // eg. 'mypurecloud.ie', 'euw2.pure.cloud', etc...
+        region: 'apne2.pure.cloud'
+    },
+
+    translateServiceURI: 'https://tkohdev.github.io/translate/'
+} 
